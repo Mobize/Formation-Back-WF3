85ba9f4841a87af20a6f32f3cbc1b7f6bc07f2a1
Mobize <34886414+Mobize@users.noreply.github.com>
2018-02-17 08:36:03 +0100

    portfolio 2018

---
+++ Portfolio 2018/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Olivier Charpentier</title>
+	<style>
+			@import url('https://fonts.googleapis.com/css?family=Raleway');
+	</style>
+	<link rel="stylesheet" href="./bootstrap-3.3.7-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./style.css">
+    <style>
+         @import url('https://fonts.googleapis.com/css?family=Raleway:500');
+         @import url('https://fonts.googleapis.com/css?family=Poiret+One');
+    </style>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row first">
+            <div class="col-md-12 portable">
+                <img class="background img-responsive" src="./Background_portable2.jpg" alt="ordinateur portable" class="background">
+            </div><!-- col md 12 portable -->
+        </div><!-- fin de la row first-->
+        <div class="row text">
+		    <div class="col-md-9 col-sm-9 col-xs-8 text-background">
+			    <h1>Olivier Charpentier</h1>
+                <h2 class="text-under">Développeur Intégrateur Web</h2>
+            </div>  
+        </div> <!-- row text --> 
+        <div class="row nav ">
+            <div class="col-lg-6 navi">
+                <nav id="bloc2" class="navbar navbar-default ">
+                    <div class="container-fluid">
+                        <!-- Brand and toggle get grouped for better mobile display -->
+                        <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Olivier</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">Menu</a>
+                        </div>
+                        <!-- Collect the nav links, forms, and other content for toggling -->
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#presentation">Présentation <span class="sr-only">(current)</span></a></li>
+                            <li class="active"><a href="#competences">Compétences</a></li>
+                            <li class="active"><a href="#realisations">Réalisations</a></li>
+                            <li class="active"><a href="#parcours">Parcours</a></li>
+                            <li class="active"><a href="#contact">Contact</a></li>
+                        </ul>
+                        </div><!-- /.navbar-collapse -->
+                    </div><!-- /.container-fluid -->
+                </nav>
+            </div><!-- col lg 12 -->
+        </div><!-- row nav -->
+    </div><!-- fin du container-fluid -->
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <p id="presentation">presentation</p> 
+                <img src="./portrait2.gif" class="img-responsive portrait" alt="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel eros sit amet velit mattis lacinia. Proin volutpat aliquam massa, pulvinar lobortis nisl fermentum et. Suspendisse dui neque, cursus ut luctus non, aliquam vel lectus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam efficitur mi sit amet risus accumsan consectetur. Nunc nec lacinia nunc. Nullam pulvinar porttitor auctor. Praesent tempus at ante vel feugiat. Nunc tempus leo in mauris mattis tincidunt. Pellentesque eget rutrum augue. Donec ac turpis ultricies, mollis est in, dapibus metus. In non lorem sit amet mauris porta semper id quis diam.
+
+Fusce sit amet quam leo. Morbi elit ante, blandit a blandit a, efficitur in tortor. Maecenas ut nunc vehicula lectus aliquet maximus sed ut orci. Aliquam erat volutpat. Sed venenatis nulla ac neque congue convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In hac habitasse platea dictumst. Curabitur vel volutpat nunc, non rutrum neque. Nunc pretium ante urna, eget consequat nisl accumsan vel. Maecenas bibendum felis justo, eget condimentum orci ultrices a. Vestibulum malesuada lacus id mattis tempus. Curabitur eu fringilla nisl. Proin id quam pharetra, malesuada elit quis, imperdiet mauris. Suspendisse potenti.
+
+Fusce erat nibh, efficitur in neque pellentesque, venenatis rutrum nulla. Aliquam posuere ipsum at semper molestie. In iaculis neque commodo massa pellentesque euismod. Donec nec imperdiet ex, vitae tristique ipsum. Morbi feugiat, ipsum a rhoncus rhoncus, mi purus ultricies ante, at accumsan ex nibh scelerisque odio. Morbi ut enim rutrum ante sollicitudin blandit. Donec et ex orci. Donec et fringilla magna. Sed lacinia rutrum sem, ac ornare tortor tincidunt in. Vestibulum orci nunc, sagittis et lorem sed, congue viverra eros. Aliquam erat volutpat. Ut eu metus ornare, dignissim erat at, commodo mi. In vel quam in nunc viverra posuere ac ut urna. Nunc blandit malesuada tortor eu aliquet. Etiam tincidunt lectus sed felis ultrices vulputate. Proin malesuada accumsan rhoncus.
+
+Nam pretium sed mauris ut congue. Duis eget consequat nisl, at tristique ipsum. Nam cursus congue urna, et ornare felis suscipit quis. Praesent consequat sit amet lectus at finibus. Fusce ultrices maximus nunc ut sagittis. Etiam dolor turpis, fermentum ac luctus sed, facilisis et lorem. Aliquam a mi purus. Quisque in nulla non dolor sodales dictum. Pellentesque auctor congue urna. Nam varius nec nibh sed facilisis. Nam augue tortor, consequat et lobortis non, finibus at tortor. Suspendisse et sodales orci, vel consectetur nunc.
+
+Nulla facilisi. Vivamus non tristique purus, at dapibus ligula. Cras ut massa sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut laoreet vitae nisl ac elementum. Cras sagittis nulla vel magna fermentum, nec commodo mauris cursus. Duis viverra, lectus vitae efficitur pretium, ex turpis posuere arcu, aliquet finibus libero massa quis urna.
+
+Proin vel congue nunc. Proin mauris est, condimentum ut sodales ac, fringilla at nulla. In sodales sapien ut nibh cursus sodales. Etiam velit ipsum, fringilla dignissim lorem a, sollicitudin euismod sapien. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Phasellus at pretium tellus. Aenean vestibulum tincidunt arcu, ac auctor tellus pretium et. Sed tempor, sem non lobortis laoreet, mauris neque tristique mi, quis tincidunt nibh est in quam. Sed non urna iaculis, suscipit lorem eu, dignissim neque. Nulla eget nunc elit. Proin sed hendrerit est. Nulla lobortis urna id enim rhoncus eleifend. Sed sodales, nibh quis sollicitudin dignissim, lorem nisl hendrerit urna, non suscipit neque augue nec enim.
+
+Duis vel lacus non turpis aliquam aliquam. Suspendisse potenti. Ut et elit sit amet velit sagittis tincidunt. Curabitur vestibulum, risus et efficitur tempus, odio ante placerat quam, non ultrices orci neque vel nulla. Donec sed dui lectus. Nunc at felis accumsan, condimentum nulla ac, vehicula arcu. Curabitur risus justo, tincidunt vitae metus eu, luctus accumsan lacus. Pellentesque fringilla magna sit amet pulvinar blandit. Nunc mauris ante, venenatis ut cursus sit amet, cursus mattis orci. Vivamus at massa commodo, consectetur ex sed, varius sapien. Cras feugiat ac lacus nec volutpat. Suspendisse cursus fringilla lacus, vitae ultrices arcu gravida at.
+
+Proin blandit magna massa, eget imperdiet libero consectetur et. Maecenas ac sem ut orci posuere rutrum nec eget diam. Sed at felis nec dolor malesuada sagittis vel a tortor. Morbi euismod risus nec neque vestibulum, vitae rutrum dolor posuere. In posuere, ipsum id vehicula auctor, ipsum magna placerat diam, non pharetra dolor arcu ut neque. Vestibulum ullamcorper orci at pretium tempor. Duis sapien est, interdum ac rutrum vitae, blandit sed purus. Quisque eget cursus enim, eu mollis ligula. Suspendisse facilisis ut orci molestie scelerisque. Pellentesque ultricies lectus ac eros pretium, ut dapibus libero dapibus. Maecenas iaculis suscipit eleifend. Phasellus non rutrum nulla. Vivamus mollis ex ac placerat suscipit. Sed nibh orci, egestas non vehicula ac, fermentum vitae diam. Curabitur pulvinar risus non tincidunt egestas.
+
+Quisque cursus eget felis a vulputate. Sed feugiat maximus mi et fringilla. Aenean id efficitur eros. Sed facilisis elit id ligula aliquet euismod. Cras suscipit nec justo a maximus. Nam ipsum magna, pharetra non interdum at, rhoncus ac nunc. Etiam risus ipsum, tempus eu laoreet vitae, varius quis quam. Praesent quis posuere mi. Pellentesque ac pharetra felis. Vivamus lorem odio, sodales euismod nulla in, finibus tincidunt metus. Integer nec lorem in augue placerat facilisis a eu massa.
+
+Aliquam vitae iaculis massa. Vivamus congue odio id risus ultricies auctor. Praesent vel ex id ante accumsan posuere eget at felis. Suspendisse interdum, nibh quis accumsan aliquet, nulla massa posuere dolor, scelerisque fringilla lectus massa id elit. Vestibulum a turpis aliquam, faucibus enim id, mattis eros. Donec lobortis risus a gravida faucibus. Aenean in accumsan turpis. Donec neque leo, aliquam id nisl at, consequat aliquet </p>
+            </div>
+        </div>              
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p id="competences">Competences</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p id="realisations">realisations</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p id="parcours">parcours</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p id="contact">contact</p>
+            </div>
+        </div>
+    </div>
+<footer>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+
+<script>
+  $(document).ready(function(){
+    $("#bloc2").sticky({topSpacing:0});
+  });
+</script>
+
+<script type="text/javascript">
+
+
+
+/* var iScrollPos = 0;
+ 
+  
+ 
+ $(window).scroll(function () {
+ 
+     var iCurScrollPos = $(this).scrollTop();
+ 
+     if (iCurScrollPos > iScrollPos) {
+ 
+        $('#bloc2').animate({'top' : '0px'}, {duration: 10});
+ 
+     } else {
+ 
+        $('#bloc2').animate({'top' : '313px'}, {duration: 10}); 
+ 
+     }
+ 
+     iScrollPos = iCurScrollPos;
+ 
+ });
+ */
+
+
+
+
+
+
+/*     $(document).ready(function(){
+    
+
+  
+$(window).scroll(function(){
+	posScroll = $(document).scrollTop();
+	if(posScroll >=250) 
+		$('#bloc2').fadeIn(600);
+	else
+		$('#bloc2').fadeOut(600);
+});
+    
+    }); */
+ </script>
+
+<!-- bouton haut -->
+<script>            
+    jQuery(document).ready(function() {
+    var duration = 500;
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 100) {
+        // Si un défillement de 100 pixels ou plus.
+        // Ajoute le bouton
+        jQuery('.cRetour').fadeIn(duration);
+        } else {
+        // Sinon enlève le bouton
+        jQuery('.cRetour').fadeOut(duration);
+        }
+    });
+                    
+    jQuery('.cRetour').click(function(event) {
+        // Un clic provoque le retour en haut animé.
+        event.preventDefault();
+        jQuery('html, body').animate({scrollTop: 0}, duration);
+       /*  $('#bloc2').animate({'top' : '313px'}, {duration: 400}); */
+        return false;
+    })
+    });
+</script>
+
+<div class="cRetour"></div>
+    <script src="./bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./mesfonctions.js"></script>
+    <script type="text/javascript" src="./jquery.sticky.js"></script>
+  
+</footer>
+</body>
+</html>
